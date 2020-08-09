089609926b62f0cbf45f8242b88b8df419d161e1
xs <1183315612@qq.com>
2020-08-09 21:03:09 +0800

    shop-portal

---
+++ src/main/java/com/fh/shop/portal/ShopPortalApplication.java
@@ -0,0 +1,13 @@
+package com.fh.shop.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fh/shop/portal/ShopPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.shop.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
